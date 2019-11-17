10f44b9fdb54b84beff940eb780d7b57de0c533c
hey-kong <iggiewang@gmail.com>
2019-11-17 16:33:16 +0800

    finish 217-containsDuplicate

---
+++ containsDuplicate.go
@@ -0,0 +1,13 @@
+package main
+
+// Leetcode 217. (easy)
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]bool)
+	for _, num := range nums {
+		if _, ok := m[num]; ok {
+			return true
+		}
+		m[num] = true
+	}
+	return false
+}
