e81124f823d025657e2d8ab1f0e00bb93b4d94f1
hey-kong <iggiewang@gmail.com>
2019-11-25 10:53:15 +0800

    finish 191-hammingWeight

--- hammingWeight.go
+++ hammingWeight.go
@@ -1,14 +1,20 @@
 package main
 
 // Leetcode 191. (easy)
-func hammingWeight(num uint32) int {
+func hammingWeight1(num uint32) int {
 	bits := 0
-	var mask uint32 = 1
-	for i := 0; i < 32; i++ {
-		if num & mask != 0 {
-			bits++
-		}
-		mask <<= 1
+	for num != 0 {
+		bits++
+		num = num & (num - 1)
 	}
 	return bits
+}
+
+func hammingWeight2(num uint32) int {
+	num = (num&0x55555555) + (num>>1&0x55555555)
+	num = (num&0x33333333) + (num>>2&0x33333333)
+	num = (num&0x0F0F0F0F) + (num>>4&0x0F0F0F0F)
+	num = (num&0x00FF00FF) + (num>>8&0x00FF00FF)
+	num = (num&0x0000FFFF) + (num>>16&0x0000FFFF)
+	return int(num)
 }
