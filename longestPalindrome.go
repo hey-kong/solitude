fc5d699f4cede20f9085a00eb7ac7d9c874197c5
hey-kong <iggiewang@gmail.com>
2019-12-18 16:26:24 +0800

    finish 5-longestPalindrome

---
+++ longestPalindrome.go
@@ -0,0 +1,31 @@
+package main
+
+// Leetcode 5. (medium)
+func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	start := 0
+	end := 0
+	for i := range s {
+		l1, r1 := findPalindrome(s, i-1, i+1)
+		l2, r2 := findPalindrome(s, i, i+1)
+		if (r1 - l1 > r2 - l2) && (r1 - l1 > end - start) {
+			start = l1
+			end = r1
+		} else if (r2 - l2 > r1 - l1) && (r2 - l2 > end - start) {
+			start = l2
+			end = r2
+		}
+	}
+	return s[start:end+1]
+}
+
+func findPalindrome(s string, l, r int) (int, int) {
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l--
+		r++
+	}
+	return l+1, r-1
+}
