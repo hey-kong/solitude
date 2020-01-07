d5d650274652eedd9c4c0be7a38d6cbcef789391
hey-kong <iggiewang@gmail.com>
2020-01-07 18:03:50 +0800

    finish 338-countBits

---
+++ countBits.go
@@ -0,0 +1,10 @@
+package main
+
+// Leetcode 338. (medium)
+func countBits(num int) []int {
+	res := make([]int, num+1)
+	for i := 1; i <= num; i++ {
+		res[i] = res[i&(i-1)] + 1
+	}
+	return res
+}
