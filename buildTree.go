307966db5327661635e1b70aa9f4625c51a6c6e0
hey-kong <iggiewang@gmail.com>
2020-01-14 13:11:44 +0800

    finish 106-buildTree

--- buildTree.go
+++ buildTree.go
@@ -25,3 +25,29 @@ func recursiveBuildTree(preorder []int, preL, preR int, inMap map[int]int, inL i
 	node.Right = recursiveBuildTree(preorder, preL+leftCount+1, preR, inMap, i+1)
 	return node
 }
+
+// Leetcode 106. (medium)
+func buildTree2(inorder []int, postorder []int) *TreeNode {
+	inMap := make(map[int]int)
+	for i, num := range inorder {
+		inMap[num] = i
+	}
+	return recursiveBuildTree2(postorder, 0, len(postorder)-1, inMap, len(inorder)-1)
+}
+
+func recursiveBuildTree2(postorder []int, postL, postR int, inMap map[int]int, inR int) *TreeNode {
+	if postL > postR {
+		return nil
+	}
+
+	node := &TreeNode{
+		Val:   postorder[postR],
+		Left:  nil,
+		Right: nil,
+	}
+	i := inMap[node.Val]
+	rightCount := inR - i
+	node.Left = recursiveBuildTree2(postorder, postL, postR-rightCount-1, inMap, i-1)
+	node.Right = recursiveBuildTree2(postorder, postR-rightCount, postR-1, inMap, inR)
+	return node
+}
