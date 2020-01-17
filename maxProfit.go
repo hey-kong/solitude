2f91dae21f4b283e2c2a72f611b53810f7707767
hey-kong <iggiewang@gmail.com>
2020-01-17 13:54:26 +0800

    finish 122-maxProfit

--- maxProfit.go
+++ maxProfit.go
@@ -13,3 +13,23 @@ func maxProfit(prices []int) int {
 	}
 	return res
 }
+
+// Leetcode 122. (easy)
+func maxProfit2(prices []int) int {
+	buy, sell := 0, 0
+	maxprofit := 0 
+	for i := 1; i < len(prices); i++ {
+		if prices[i] <= prices[i-1] {
+			if buy < sell {
+				maxprofit += prices[sell] - prices[buy]
+			}
+            buy = i
+		} else {
+			sell = i
+		}
+	}
+	if buy < sell {
+		maxprofit += prices[sell] - prices[buy]
+	}
+    return maxprofit
+}
