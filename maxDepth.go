b8c6025dc6faf3b7dbf09a767a494a6572929a44
hey-kong <iggiewang@gmail.com>
2020-01-27 00:40:41 +0800

    finish 104-maxDepth

---
+++ maxDepth.go
@@ -0,0 +1,16 @@
+package main
+
+// Leetcode 104. (easy)
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
