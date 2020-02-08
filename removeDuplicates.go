c5a178ef9d9502ced0e45967469f6ab9b8ba0e2e
hey-kong <iggiewang@gmail.com>
2020-02-08 14:13:12 +0800

    finish 80-removeDuplicates

--- removeDuplicates.go
+++ removeDuplicates.go
@@ -2,12 +2,30 @@ package main
 
 // Leetcode 26. (easy)
 func removeDuplicates(nums []int) int {
-	i := 0
+	mark := 0
 	for _, num := range nums {
-		if nums[i] != num {
-			i++
-			nums[i] = num
+		if nums[mark] != num {
+			mark++
+			nums[mark] = num
 		}
 	}
-	return i + 1
+	return mark + 1
+}
+
+// Leetcode 80. (medium)
+func removeDuplicates2(nums []int) int {
+	mark := 0
+	markIsSecond := false
+	for i := 1; i < len(nums); i++ {
+		if nums[mark] != nums[i] {
+			mark++
+			nums[mark] = nums[i]
+			markIsSecond = false
+		} else if !markIsSecond {
+			mark++
+			nums[mark] = nums[i]
+			markIsSecond = true
+		}
+	}
+	return mark + 1
 }
