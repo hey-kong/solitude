d20a01d500f19de581c54c707058363cd9386070
hey-kong <iggiewang@gmail.com>
2020-02-09 20:45:27 +0800

    finish 71-simplifyPath

--- simplifyPath.go
+++ simplifyPath.go
@@ -4,26 +4,16 @@ import "strings"
 
 // Leetcode 71. (medium)
 func simplifyPath(path string) string {
-	// path = "/a/../../b/../c//.//"
 	nodes := strings.Split(path, "/")
-	i := 0
-	for i < len(nodes) {
-		if nodes[i] == "" {
-			nodes = append(nodes[:i], nodes[i+1:]...)
-		} else {
-			i++
-		}
-	}
-	// nodes = [a .. .. b .. c .]
 	stack := []string{}
 	for _, node := range nodes {
-		if node == ".." && len(stack) > 0 {
-			stack = stack[:len(stack)-1]
-		} else if node != ".." && node != "." {
+		if node != "" && node != ".." && node != "." {
 			stack = append(stack, node)
+		} else if node == ".." && len(stack) > 0 {
+			stack = stack[:len(stack)-1]
 		}
 	}
-	// stack = [c]
+
 	if len(stack) == 0 {
 		return "/"
 	}
