b5c195ebe127c7b72aad8fa48fdb88626c222409
hey-kong <iggiewang@gmail.com>
2020-02-28 10:23:21 +0800

    finish 229-majorityElement

--- majorityElement.go
+++ majorityElement.go
@@ -18,3 +18,41 @@ func majorityElement(nums []int) int {
 	}
 	return res
 }
+
+// Leetcode 229. (medium)
+func majorityElement2(nums []int) []int {
+	a, b := 1<<31-1, 1<<31-1
+	cnta, cntb := 0, 0
+	for _, num := range nums {
+		if num == a {
+			cnta++
+		} else if num == b {
+			cntb++
+		} else if cnta == 0 {
+			a = num
+			cnta = 1
+		} else if cntb == 0 {
+			b = num
+			cntb = 1
+		} else {
+			cnta--
+			cntb--
+		}
+	}
+	cnta, cntb = 0, 0
+	for _, num := range nums {
+		if num == a {
+			cnta++
+		} else if num == b {
+			cntb++
+		}
+	}
+	res := []int{}
+	if cnta > len(nums)/3 {
+		res = append(res, a)
+	}
+	if cntb > len(nums)/3 {
+		res = append(res, b)
+	}
+	return res
+}
