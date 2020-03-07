05a0d0000b8cc69d7df87b0a10ac89e5a4addcd8
hey-kong <iggiewang@gmail.com>
2020-03-07 17:17:04 +0800

    finish 155-MinStack

---
+++ MinStack.go
@@ -0,0 +1,44 @@
+package main
+
+// Leetcode 155. (easy)
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{[]int{}, []int{}}
+}
+
+func (this *MinStack) Push(x int) {
+	this.stack = append(this.stack, x)
+	if len(this.minStack) == 0 || this.minStack[len(this.minStack)-1] >= x {
+		this.minStack = append(this.minStack, x)
+	}
+}
+
+func (this *MinStack) Pop() {
+	val := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	if val == this.minStack[len(this.minStack)-1] {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
