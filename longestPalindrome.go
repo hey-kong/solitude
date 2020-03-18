1c8b452fd492acc071395ef61852782525a13aed
hey-kong <iggiewang@gmail.com>
2020-03-19 00:19:37 +0800

    finish 409-longestPalindrome

--- longestPalindrome.go
+++ longestPalindrome.go
@@ -11,15 +11,15 @@ func longestPalindrome(s string) string {
 	for i := range s {
 		l1, r1 := findPalindrome(s, i-1, i+1)
 		l2, r2 := findPalindrome(s, i, i+1)
-		if (r1 - l1 > r2 - l2) && (r1 - l1 > end - start) {
+		if (r1-l1 > r2-l2) && (r1-l1 > end-start) {
 			start = l1
 			end = r1
-		} else if (r2 - l2 > r1 - l1) && (r2 - l2 > end - start) {
+		} else if (r2-l2 > r1-l1) && (r2-l2 > end-start) {
 			start = l2
 			end = r2
 		}
 	}
-	return s[start:end+1]
+	return s[start : end+1]
 }
 
 func findPalindrome(s string, l, r int) (int, int) {
@@ -27,5 +27,24 @@ func findPalindrome(s string, l, r int) (int, int) {
 		l--
 		r++
 	}
-	return l+1, r-1
+	return l + 1, r - 1
+}
+
+// Leetcode 409. (easy)
+func longestPalindrome2(s string) int {
+	charCnt := make([]int, 58)
+	for _, char := range s {
+		charCnt[char-'A']++
+	}
+	res := 0
+	resIsOdd := 0
+	for _, num := range charCnt {
+		if num%2 == 0 {
+			res += num
+		} else {
+			res += num / 2 * 2
+			resIsOdd = 1
+		}
+	}
+	return res + resIsOdd
 }
