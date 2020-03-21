198bf6232da436dc388e9760d0d4f51d6e028c62
hey-kong <iggiewang@gmail.com>
2020-03-21 14:53:52 +0800

    finish 59-generateMatrix

--- generateMatrix.go
+++ generateMatrix.go
@@ -45,3 +45,42 @@ func generateMatrix(n int) [][]int {
 	}
 	return matrix
 }
+
+func generateMatrix(n int) [][]int {
+	matrix := make([][]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			idx := getCircleIdx(i, j, n)
+			out := 0
+			for k := 0; k < idx; k++ {
+				out += getCircleNumCnt(k, n)
+			}
+			cur := getCircleNumCnt(idx, n)
+			if i == idx {
+				matrix[i][j] = out + 1 + (j - idx)
+			} else if j >= i {
+				matrix[i][j] = out + (n - 2*idx) + (i - idx)
+			} else if j == idx {
+				matrix[i][j] = out + cur + 1 - (i - idx)
+			} else {
+				matrix[i][j] = out + cur/4*3 + 1 - (j - idx)
+			}
+		}
+	}
+	return matrix
+}
+
+func getCircleIdx(i, j, n int) int {
+	return min(min(i, j), n-1-max(i, j))
+}
+
+func getCircleNumCnt(idx, n int) int {
+	if res := (n - 2*idx - 1) * 4; res > 0 {
+		return res
+	}
+	return 1
+}
