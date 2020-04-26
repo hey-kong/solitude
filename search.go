fb5b3986d0a881c91e41bb6b1acdbbd908add39b
hey-kong <iggiewang@gmail.com>
2020-04-27 01:51:47 +0800

    finish 33-search

--- search.go
+++ search.go
@@ -6,9 +6,9 @@ func search(nums []int, target int) int {
 		return -1
 	}
 
-	left, right := 0, len(nums) - 1
+	left, right := 0, len(nums)-1
 	for left <= right {
-		mid := left + (right - left) / 2
+		mid := left + (right-left)/2
 		if nums[mid] == nums[right] {
 			break
 		} else if nums[mid] > nums[right] {
@@ -24,13 +24,36 @@ func search(nums []int, target int) int {
 		left, right = 0, right-1
 	}
 	for left <= right {
-		mid := left + (right - left) / 2
+		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
 		} else if nums[mid] > target {
 			right = mid - 1
 		} else {
 			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func search2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < nums[right] {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
 		}
 	}
 	return -1
