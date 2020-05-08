dc4776a4c3a67a2933f17651d06aabeb90a42c6b
hey-kong <iggiewang@gmail.com>
2020-05-08 11:27:28 +0800

    finish 221-maximalSquare

--- maximalSquare.go
+++ maximalSquare.go
@@ -2,18 +2,20 @@ package main
 
 // Leetcode 221. (medium)
 func maximalSquare(matrix [][]byte) int {
-	if len(matrix) == 0 {
-		return 0
-	}
-	dp := make([][]int, len(matrix)+1)
+	dp := make([][]int, len(matrix))
 	for i := range dp {
-		dp[i] = make([]int, len(matrix[0])+1)
+		dp[i] = make([]int, len(matrix[0]))
 	}
+
 	maxSquare := 0
-	for i := 1; i <= len(matrix); i++ {
-		for j := 1; j <= len(matrix[0]); j++ {
-			if matrix[i-1][j-1] == '1' {
-				dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if matrix[i][j] == '1' {
+				if i == 0 || j == 0 {
+					dp[i][j] = 1
+				} else {
+					dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1
+				}
 				maxSquare = max(maxSquare, dp[i][j]*dp[i][j])
 			}
 		}
