7a8661e45232f24a77ceeb67832aeca828741880
hey-kong <iggiewang@gmail.com>
2020-05-11 14:15:24 +0800

    finish 40-combinationSum2

--- combinationSum.go
+++ combinationSum.go
@@ -28,3 +28,34 @@ func recursiveCombinationSum(candidates []int, target, cur, begin int, tmp []int
 	}
 	return res
 }
+
+// Leetcode 40. (medium)
+func combinationSum2(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	return recursiveCombinationSum2(candidates, target, 0, 0, []int{}, [][]int{})
+}
+
+func recursiveCombinationSum2(candidates []int, target, cur, begin int, tmp []int, res [][]int) [][]int {
+	if cur == target {
+		arr := make([]int, len(tmp))
+		copy(arr, tmp)
+		res = append(res, arr)
+		return res
+	}
+
+	for i := begin; i < len(candidates); i++ {
+		if i > begin && candidates[i-1] == candidates[i] {
+			continue
+		}
+
+		tmp = append(tmp, candidates[i])
+		cur += candidates[i]
+		if cur > target {
+			return res
+		}
+		res = recursiveCombinationSum2(candidates, target, cur, i+1, tmp, res)
+		tmp = tmp[:len(tmp)-1]
+		cur -= candidates[i]
+	}
+	return res
+}
