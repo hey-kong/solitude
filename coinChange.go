1d6cfd0d2b2ca0a442bb726b33abf029393f7cf1
hey-kong <iggiewang@gmail.com>
2020-06-19 16:22:16 +0800

    finish 322-coinChange

--- coinChange.go
+++ coinChange.go
@@ -6,11 +6,9 @@ func coinChange(coins []int, amount int) int {
 	for i := 1; i <= amount; i++ {
 		dp[i] = amount + 1
 	}
-	for i := 1; i <= amount; i++ {
-		for _, coin := range coins {
-			if last := i - coin; last >= 0 {
-				dp[i] = min(dp[i], dp[last]+1)
-			}
+	for _, coin := range coins {
+		for i := coin; i <= amount; i++ {
+			dp[i] = min(dp[i], dp[i-coin]+1)
 		}
 	}
 	if dp[amount] > amount {
