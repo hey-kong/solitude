e118e93dfc5087aa62027c6367965328637733e2
hey-kong <iggiewang@gmail.com>
2020-09-30 00:18:59 +0800

    finish 113-pathSum

--- pathSum.go
+++ pathSum.go
@@ -20,3 +20,24 @@ func recursivePathSum(root *TreeNode, sum int) int {
 	res += recursivePathSum(root.Right, sum-root.Val)
 	return res
 }
+
+// Leetcode 113. (medium)
+func pathSum2(root *TreeNode, sum int) [][]int {
+	return recursivePathSum2(root, sum, []int{}, [][]int{})
+}
+
+func recursivePathSum2(root *TreeNode, sum int, cur []int, res [][]int) [][]int {
+	if root == nil {
+		return res
+	}
+	cur = append(cur, root.Val)
+	if root.Val == sum && root.Left == nil && root.Right == nil {
+		tmp := make([]int, len(cur))
+		copy(tmp, cur)
+		res = append(res, tmp)
+	}
+	res = recursivePathSum2(root.Left, sum-root.Val, cur, res)
+	res = recursivePathSum2(root.Right, sum-root.Val, cur, res)
+	cur = cur[:len(cur)-1]
+	return res
+}
