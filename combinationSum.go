97bd49c62127b5141aa20574baddf4d4a29204df
hey-kong <iggiewang@gmail.com>
2020-10-13 02:06:04 +0800

    finish 377-combinationSum4

--- combinationSum.go
+++ combinationSum.go
@@ -59,3 +59,18 @@ func recursiveCombinationSum2(candidates []int, target, cur, begin int, tmp []in
 	}
 	return res
 }
+
+// Leetcode 377. (medium)
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+
+	for i := 1; i <= target; i++ {
+		for _, num := range nums {
+			if i >= num {
+				dp[i] += dp[i-num]
+			}
+		}
+	}
+	return dp[target]
+}
