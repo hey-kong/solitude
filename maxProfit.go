edf4751880f485096e659888708298159eaf8e8d
hey-kong <iggiewang@gmail.com>
2020-10-22 20:07:23 +0800

    finish 309-maxProfitWithCooldown

--- maxProfit.go
+++ maxProfit.go
@@ -24,3 +24,22 @@ func maxProfit2(prices []int) int {
 	}
 	return res
 }
+
+// Leetcode 309. (medium)
+func maxProfitWithCooldown(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	dp := make([][]int, len(prices))
+	for i := range dp {
+		dp[i] = make([]int, 3)
+	}
+	dp[0][0] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][2])
+		dp[i][2] = dp[i-1][0] + prices[i]
+	}
+	return max(dp[len(prices)-1][1], dp[len(prices)-1][2])
+}
