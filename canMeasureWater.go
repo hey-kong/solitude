493428057cb4a2bac6bba425f2574e11626fb41c
hey-kong <iggiewang@gmail.com>
2020-10-30 18:19:49 +0800

    finish 365-canMeasureWater

--- canMeasureWater.go
+++ canMeasureWater.go
@@ -5,10 +5,10 @@ func canMeasureWater(x int, y int, z int) bool {
 	if z > x+y {
 		return false
 	}
-	if z == 0 {
+	if z == 0 || z == x || z == y {
 		return true
 	}
-
+	// ax + by = z (贝祖定理)
 	if gcd(x, y) != 0 && z%gcd(x, y) == 0 {
 		return true
 	}
