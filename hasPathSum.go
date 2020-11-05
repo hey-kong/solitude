c8f9f1984089a6b22e15dc04f5440a9297907b19
hey-kong <iggiewang@gmail.com>
2020-11-05 20:54:48 +0800

    finish 112-hasPathSum

---
+++ hasPathSum.go
@@ -0,0 +1,12 @@
+package main
+
+// Leetcode 112. (medium)
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == sum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
