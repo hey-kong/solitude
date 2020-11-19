bb3523f683359c7045bb5bda1d2a2161f20e39a8
hey-kong <iggiewang@gmail.com>
2020-11-19 13:02:12 +0800

    finish 167-twoSum

--- twoSum.go
+++ twoSum.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/sqs/goreturns/returns"
+
 // Leetcode 1. (easy)
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
@@ -14,4 +16,19 @@ func twoSum(nums []int, target int) []int {
 		m[num] = i
 	}
 	return res
+}
+
+// Leetcode 167. (easy)
+func twoSum2(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for i < j {
+		if numbers[i]+numbers[j] == target {
+			return []int{i + 1, j + 1}
+		} else if numbers[i]+numbers[j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return []int{-1, -1}
 }
