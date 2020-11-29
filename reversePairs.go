d1a87ff8d53ae9aece7b8b8c7479e922a64af4cc
hey-kong <iggiewang@gmail.com>
2020-11-29 18:21:13 +0800

    finish 493-reversePairs

--- reversePairs.go
+++ reversePairs.go
@@ -1,6 +1,6 @@
 package main
 
-// Leetcode m51. (hard)
+// Leetcode 493. (hard)
 func reversePairs(nums []int) int {
 	aux := make([]int, len(nums))
 	return mergeSortOfReversePairs(nums, aux, 0, len(nums)-1)
@@ -13,32 +13,41 @@ func mergeSortOfReversePairs(nums, aux []int, left, right int) int {
 	mid := left + (right-left)/2
 	ans1 := mergeSortOfReversePairs(nums, aux, left, mid)
 	ans2 := mergeSortOfReversePairs(nums, aux, mid+1, right)
-	ans3 := mergeOfReversePairs(nums, aux, left, mid, right)
+	i, j := left, mid+1
+	ans3 := 0
+	for i <= mid {
+		for j <= right && nums[i] > 2*nums[j] {
+			ans3 += mid - i + 1
+			j++
+		}
+		i++
+	}
+	mergeArray(nums, aux, left, mid, right)
 	return ans1 + ans2 + ans3
 }
 
-func mergeOfReversePairs(nums, aux []int, left, mid, right int) int {
+// Leetcode m51. (hard)
+func reversePairs2(nums []int) int {
+	aux := make([]int, len(nums))
+	return mergeSortOfReversePairs2(nums, aux, 0, len(nums)-1)
+}
+
+func mergeSortOfReversePairs2(nums, aux []int, left, right int) int {
+	if left >= right {
+		return 0
+	}
+	mid := left + (right-left)/2
+	ans1 := mergeSortOfReversePairs2(nums, aux, left, mid)
+	ans2 := mergeSortOfReversePairs2(nums, aux, mid+1, right)
 	i, j := left, mid+1
-	ans := 0
-	for k := left; k <= right; k++ {
-		aux[k] = nums[k]
-	}
-	for k := left; k <= right; k++ {
-		if i > mid {
-			nums[k] = aux[j]
+	ans3 := 0
+	for i <= mid {
+		for j <= right && nums[i] > nums[j] {
+			ans3 += mid - i + 1
 			j++
-		} else if j > right {
-			nums[k] = aux[i]
-			ans += right - mid
-			i++
-		} else if aux[j] < aux[i] {
-			nums[k] = aux[j]
-			j++
-		} else {
-			nums[k] = aux[i]
-			ans += j - mid - 1
-			i++
 		}
+		i++
 	}
-	return ans
+	mergeArray(nums, aux, left, mid, right)
+	return ans1 + ans2 + ans3
 }
