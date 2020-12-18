0f9432fed6f43c2bd11e5d63b2e5cd4d858f247d
hey-kong <iggiewang@gmail.com>
2020-12-19 01:06:17 +0800

    finish 48-rotate

--- rotate.go
+++ rotate.go
@@ -1,27 +1,42 @@
 package main
 
+// Leetcode 48. (medium)
+func rotateImage(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+	}
+}
+
 // Leetcode 189. (easy)
-func rotate(nums []int, k int)  {
+func rotate(nums []int, k int) {
 	if k >= len(nums) {
-		k %= len(nums);
+		k %= len(nums)
 	}
 	if k == 0 {
-		return;
+		return
 	}
 
-	i := 0;
-	start := 0;
-	tmp := nums[i];
+	i := 0
+	start := 0
+	tmp := nums[i]
 	for _ = range nums {
-		i += k;
+		i += k
 		if i >= len(nums) {
-			i -= len(nums);
+			i -= len(nums)
 		}
-		tmp, nums[i] = nums[i], tmp;
+		tmp, nums[i] = nums[i], tmp
 
 		if i == start {
-			i++;
-			start = i;
+			i++
+			start = i
 			tmp = nums[i]
 		}
 	}
