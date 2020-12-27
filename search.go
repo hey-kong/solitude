7014163139b1c983b7a3169fdcfa68f056808c66
hey-kong <iggiewang@gmail.com>
2020-12-27 21:43:00 +0800

    finish 81-search

--- search.go
+++ search.go
@@ -58,3 +58,31 @@ func search2(nums []int, target int) int {
 	}
 	return -1
 }
+
+// Leetcode 81. (medium)
+func search3(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return true
+		} else if nums[mid] == nums[right] {
+			right--
+		} else if nums[mid] < nums[right] {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else if nums[left] == nums[mid] {
+			left++
+		} else {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+	return false
+}
