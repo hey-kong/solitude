8e1312774da314cace7357bc059fb49fce64bf19
hey-kong <iggiewang@gmail.com>
2021-01-02 01:41:08 +0800

    finish 123-maxProfit

--- maxProfit.go
+++ maxProfit.go
@@ -25,32 +25,9 @@ func maxProfit2(prices []int) int {
 	return res
 }
 
-// Leetcode 309. (medium)
-func maxProfitWithCooldown(prices []int) int {
-	if len(prices) <= 1 {
-		return 0
-	}
-
-	dp := make([][]int, len(prices))
-	for i := range dp {
-		dp[i] = make([]int, 3)
-	}
-	dp[0][0] = -prices[0]
-	for i := 1; i < len(prices); i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
-		dp[i][1] = max(dp[i-1][1], dp[i-1][2])
-		dp[i][2] = dp[i-1][0] + prices[i]
-	}
-	return max(dp[len(prices)-1][1], dp[len(prices)-1][2])
-}
-
-// Leetcode 714. (medium)
-func maxProfit3(prices []int, fee int) int {
-	hold, sell := -prices[0], 0
-	for i := 1; i < len(prices); i++ {
-		hold, sell = max(hold, sell-prices[i]), max(sell, hold+prices[i]-fee)
-	}
-	return max(hold, sell)
+// Leetcode 123. (hard)
+func maxProfit3(prices []int) int {
+	return maxProfit4(2, prices)
 }
 
 // Leetcode 188. (hard)
@@ -82,4 +59,32 @@ func maxProfit4(k int, prices []int) int {
 		res = max(res, sell[i][len(prices)-1])
 	}
 	return res
+}
+
+// Leetcode 309. (medium)
+func maxProfitWithCooldown(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	dp := make([][]int, len(prices))
+	for i := range dp {
+		dp[i] = make([]int, 3)
+	}
+	dp[0][0] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][2])
+		dp[i][2] = dp[i-1][0] + prices[i]
+	}
+	return max(dp[len(prices)-1][1], dp[len(prices)-1][2])
+}
+
+// Leetcode 714. (medium)
+func maxProfitWithFee(prices []int, fee int) int {
+	hold, sell := -prices[0], 0
+	for i := 1; i < len(prices); i++ {
+		hold, sell = max(hold, sell-prices[i]), max(sell, hold+prices[i]-fee)
+	}
+	return max(hold, sell)
 }
