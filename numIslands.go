b1b61e232e85c49dd9b71bf4cd84a1920986ded7
hey-kong <iggiewang@gmail.com>
2021-01-10 18:09:09 +0800

    finish 200-numIslands

--- numIslands.go
+++ numIslands.go
@@ -1,33 +1,63 @@
 package main
 
 // Leetcode 200. (medium)
-func numIslands(grid [][]byte) int {
+func numIslands(grid [][]byte) (res int) {
+	if len(grid) == 0 {
+		return
+	}
+
+	mr, mc := len(grid), len(grid[0])
+	for i := 0; i < mr; i++ {
+		for j := 0; j < mc; j++ {
+			if grid[i][j] == '1' {
+				res++
+				dfsNumIslands(grid, i, j)
+			}
+		}
+	}
+	return
+}
+
+func dfsNumIslands(grid [][]byte, r, c int) {
+	mr, mc := len(grid), len(grid[0])
+	if r == -1 || r == mr || c == -1 || c == mc || grid[r][c] == '0' {
+		return
+	}
+	grid[r][c] = '0'
+	dfsNumIslands(grid, r-1, c)
+	dfsNumIslands(grid, r, c+1)
+	dfsNumIslands(grid, r+1, c)
+	dfsNumIslands(grid, r, c-1)
+}
+
+func numIslands2(grid [][]byte) (res int) {
 	if len(grid) == 0 {
 		return 0
 	}
 
+	dx := []int{-1, 0, 1, 0}
+	dy := []int{0, -1, 0, 1}
 	mr, mc := len(grid), len(grid[0])
-	num := 0
+	queue := [][2]int{}
 	for i := 0; i < mr; i++ {
 		for j := 0; j < mc; j++ {
 			if grid[i][j] == '1' {
-				num++
-				grid = dfsNumIslands(grid, i, j)
+				res++
+				grid[i][j] = '0'
+				queue = append(queue, [2]int{i, j})
+				for len(queue) > 0 {
+					x, y := queue[0][0], queue[0][1]
+					queue = queue[1:]
+					for k := 0; k < 4; k++ {
+						tmpX, tmpY := x+dx[k], y+dy[k]
+						if tmpX >= 0 && tmpX < mr && tmpY >= 0 && tmpY < mc && grid[tmpX][tmpY] == '1' {
+							grid[tmpX][tmpY] = '0'
+							queue = append(queue, [2]int{tmpX, tmpY})
+						}
+					}
+				}
 			}
 		}
 	}
-	return num
-}
-
-func dfsNumIslands(grid [][]byte, r, c int) [][]byte {
-	mr, mc := len(grid), len(grid[0])
-	if r == -1 || r == mr || c == -1 || c == mc || grid[r][c] == '0' {
-		return grid
-	}
-	grid[r][c] = '0'
-	grid = dfsNumIslands(grid, r-1, c)
-	grid = dfsNumIslands(grid, r, c+1)
-	grid = dfsNumIslands(grid, r+1, c)
-	grid = dfsNumIslands(grid, r, c-1)
-	return grid
+	return
 }
