78ac43b0a321e920bd22144b1338836ea8f9cd8c
hey-kong <iggiewang@gmail.com>
2021-01-11 12:01:54 +0800

    finish 44-isMatch

--- isMatch.go
+++ isMatch.go
@@ -30,3 +30,30 @@ func isMatch(s string, p string) bool {
 	}
 	return dp[len(s)-1][len(p)-1]
 }
+
+// Leetcode 44. (hard)
+func isMatch2(s string, p string) bool {
+	dp := make([][]bool, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]bool, len(p)+1)
+	}
+	dp[0][0] = true
+	for i := 1; i <= len(p); i++ {
+		if p[i-1] == '*' {
+			dp[0][i] = true
+		} else {
+			break
+		}
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(p); j++ {
+			if s[i-1] == p[j-1] || p[j-1] == '?' {
+				dp[i][j] = dp[i-1][j-1]
+			} else if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-1] || dp[i-1][j]
+			}
+		}
+	}
+	return dp[len(s)][len(p)]
+}
