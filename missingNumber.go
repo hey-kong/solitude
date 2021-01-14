59d76e04437e51d0330547655b74381dd56fe43b
hey-kong <iggiewang@gmail.com>
2021-01-15 01:36:37 +0800

    finish m53-missingNumber

--- missingNumber.go
+++ missingNumber.go
@@ -8,3 +8,17 @@ func missingNumber(nums []int) int {
 	}
 	return res
 }
+
+// Leetcode m53 - II. (easy)
+func missingNumber2(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == mid {
+			left = mid + 1
+		} else if nums[mid] > mid {
+			right = mid - 1
+		}
+	}
+	return left
+}
