2a191f3994d41329ed06632f8d279fe2d3fc67a2
hey-kong <iggiewang@gmail.com>
2021-01-24 01:13:07 +0800

    finish 297-Codec

--- Codec.go
+++ Codec.go
@@ -49,3 +49,65 @@ func (this *Codec) subDeserialize() *TreeNode {
 	node.Right = this.subDeserialize()
 	return node
 }
+
+// Serializes a tree to a single string.
+func (this *Codec) serialize2(root *TreeNode) string {
+	res := ""
+	queue := []*TreeNode{root}
+	for len(queue) != 0 {
+		node := queue[0]
+		queue = queue[1:]
+		if node == nil {
+			res += "nil,"
+		} else {
+			res += strconv.Itoa(node.Val) + ","
+			queue = append(queue, node.Left)
+			queue = append(queue, node.Right)
+		}
+	}
+	return res
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize2(data string) *TreeNode {
+	l := strings.Split(data[:len(data)-1], ",")
+	if l[0] == "nil" {
+		return nil
+	}
+
+	val, _ := strconv.Atoi(l[0])
+	l = l[1:]
+	root := &TreeNode{}
+	root.Val = val
+	queue := []*TreeNode{root}
+	for len(queue) != 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		if cur != nil {
+			// left
+			left := &TreeNode{}
+			if l[0] == "nil" {
+				left = nil
+			} else {
+				val, _ := strconv.Atoi(l[0])
+				left.Val = val
+			}
+			l = l[1:]
+			cur.Left = left
+			queue = append(queue, left)
+
+			// right
+			right := &TreeNode{}
+			if l[0] == "nil" {
+				right = nil
+			} else {
+				val, _ := strconv.Atoi(l[0])
+				right.Val = val
+			}
+			l = l[1:]
+			cur.Right = right
+			queue = append(queue, right)
+		}
+	}
+	return root
+}
