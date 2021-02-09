9bda0273c3b74c4c88d7495d8d4e36b4ecbacbee
hey-kong <iggiewang@gmail.com>
2021-02-09 17:15:09 +0800

    978-maxTurbulenceSize

--- maxTurbulenceSize.go
+++ maxTurbulenceSize.go
@@ -1,35 +1,17 @@
 package main
 
 // Leetcode 978. (medium)
-func maxTurbulenceSize(A []int) (res int) {
-	if len(A) <= 1 {
-		return len(A)
-	}
-
+func maxTurbulenceSize(arr []int) int {
+	res := 1
+	n := len(arr)
 	dp1, dp2 := 1, 1
-	for i := 1; i < len(A); i++ {
-		if i%2 == 0 {
-			if A[i-1] < A[i] {
-				dp1 = dp1 + 1
-				dp2 = 1
-			} else if A[i-1] > A[i] {
-				dp1 = 1
-				dp2 = dp2 + 1
-			} else {
-				dp1 = 1
-				dp2 = 1
-			}
+	for i := 1; i < n; i++ {
+		if arr[i-1] > arr[i] {
+			dp1, dp2 = dp2+1, 1
+		} else if arr[i-1] < arr[i] {
+			dp1, dp2 = 1, dp1+1
 		} else {
-			if A[i-1] > A[i] {
-				dp1 = dp1 + 1
-				dp2 = 1
-			} else if A[i-1] < A[i] {
-				dp1 = 1
-				dp2 = dp2 + 1
-			} else {
-				dp1 = 1
-				dp2 = 1
-			}
+			dp1, dp2 = 1, 1
 		}
 		res = max(res, max(dp1, dp2))
 	}
