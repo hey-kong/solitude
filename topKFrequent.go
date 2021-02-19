3f3034cab2646fd7645cf77bbc357fafc2426657
hey-kong <iggiewang@gmail.com>
2021-02-19 15:35:59 +0800

    finish 692-topKFrequent

--- topKFrequent.go
+++ topKFrequent.go
@@ -8,17 +8,14 @@ func topKFrequent(nums []int, k int) []int {
 	for _, num := range nums {
 		m[num]++
 	}
-	cntHeap := &CntHeap{}
-	heap.Init(cntHeap)
+	h := &CntHeap{}
+	heap.Init(h)
 	for num, cnt := range m {
-		heap.Push(cntHeap, [2]int{num, cnt})
-		if cntHeap.Len() > k {
-			heap.Pop(cntHeap)
-		}
+		heap.Push(h, [2]int{num, cnt})
 	}
 	res := make([]int, k)
-	for i := k - 1; i >= 0; i-- {
-		res[i] = heap.Pop(cntHeap).([2]int)[0]
+	for i := 0; i < k; i++ {
+		res[i] = (heap.Pop(h).([2]int))[0]
 	}
 	return res
 }
@@ -30,7 +27,7 @@ func (h CntHeap) Len() int {
 }
 
 func (h CntHeap) Less(i, j int) bool {
-	return h[i][1] < h[j][1]
+	return h[i][1] > h[j][1]
 }
 
 func (h CntHeap) Swap(i, j int) {
@@ -42,6 +39,56 @@ func (h *CntHeap) Push(x interface{}) {
 }
 
 func (h *CntHeap) Pop() interface{} {
+	tmp := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return tmp
+}
+
+// Leetcode 692. (medium)
+func topKFrequent2(words []string, k int) []string {
+	m := make(map[string]int)
+	for _, word := range words {
+		m[word]++
+	}
+	h := &WordCntHeap{}
+	heap.Init(h)
+	for word, cnt := range m {
+		heap.Push(h, WordCnt{word, cnt})
+	}
+	res := make([]string, k)
+	for i := 0; i < k; i++ {
+		res[i] = heap.Pop(h).(WordCnt).word
+	}
+	return res
+}
+
+type WordCnt struct {
+	word string
+	cnt  int
+}
+
+type WordCntHeap []WordCnt
+
+func (h WordCntHeap) Len() int {
+	return len(h)
+}
+
+func (h WordCntHeap) Less(i, j int) bool {
+	if h[i].cnt == h[j].cnt {
+		return h[i].word < h[j].word
+	}
+	return h[i].cnt > h[j].cnt
+}
+
+func (h WordCntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *WordCntHeap) Push(x interface{}) {
+	*h = append(*h, x.(WordCnt))
+}
+
+func (h *WordCntHeap) Pop() interface{} {
 	tmp := (*h)[len(*h)-1]
 	*h = (*h)[:len(*h)-1]
 	return tmp
