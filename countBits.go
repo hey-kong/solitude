96242de0884c731b2715fe31002f09b0f78f7afb
hey-kong <iggiewang@gmail.com>
2021-03-03 00:40:12 +0800

    338-countBits

--- countBits.go
+++ countBits.go
@@ -8,3 +8,15 @@ func countBits(num int) []int {
 	}
 	return res
 }
+
+func countBits2(num int) []int {
+	res := make([]int, num+1)
+	for i := 1; i <= num; i++ {
+		if i%2 == 1 {
+			res[i] = res[i-1] + 1
+		} else {
+			res[i] = res[i/2]
+		}
+	}
+	return res
+}
