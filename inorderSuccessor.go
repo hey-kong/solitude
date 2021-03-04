7302210ca0ea47ab740325f54cf6a53c8c75b533
hey-kong <iggiewang@gmail.com>
2021-03-04 20:26:09 +0800

    finish 285-inorderSuccessor

--- inorderSuccessor.go
+++ inorderSuccessor.go
@@ -1,7 +1,24 @@
 package main
 
+// Leetcode 285. (medium)
+func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+	return recursiveInorderSuccessor(root, p, nil)
+}
+
+func recursiveInorderSuccessor(root, p, res *TreeNode) *TreeNode {
+	if root == nil || res != nil {
+		return res
+	}
+	res = recursiveInorderSuccessor(root.Left, p, res)
+	if res == nil && root.Val > p.Val {
+		res = root
+	}
+	res = recursiveInorderSuccessor(root.Right, p, res)
+	return res
+}
+
 // Leetcode 510. (medium)
-func inorderSuccessor(node *TreeNode2) *TreeNode2 {
+func inorderSuccessor2(node *TreeNode2) *TreeNode2 {
 	if node.Right != nil {
 		node = node.Right
 		for node.Left != nil {
