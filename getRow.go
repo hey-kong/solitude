89a49cd55533bb00e2d53151e7c038a3d88ef291
hey-kong <iggiewang@gmail.com>
2021-08-05 20:22:16 +0800

    finish 119-getRow

---
+++ getRow.go
@@ -0,0 +1,13 @@
+package main
+
+// Leetcode 119. (easy)
+func getRow(rowIndex int) []int {
+	res := make([]int, rowIndex+1)
+	res[0] = 1
+	for i := 1; i <= rowIndex; i++ {
+		for j := i; j > 0; j-- {
+			res[j] += res[j-1]
+		}
+	}
+	return res
+}
