ad5060a5edf5fcbcf8f4931d45c73a7b9476c47e
hey-kong <iggiewang@gmail.com>
2022-01-11 15:45:16 +0800

    finish 125-isPalindrome

--- isPalindrome.go
+++ isPalindrome.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 // Leetcode 234. (easy)
 func isPalindrome(head *ListNode) bool {
 	slow, fast := head, head
@@ -22,4 +24,28 @@ func isPalindrome(head *ListNode) bool {
 		pre = pre.Next
 	}
 	return true
+}
+
+// Leetcode 125. (easy)
+func isPalindrome2(s string) bool {
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+	for i <= j {
+		for i <= j && !isValidOfIsPalindrome(s[i]) {
+			i++
+		}
+		for i <= j && !isValidOfIsPalindrome(s[j]) {
+			j--
+		}
+		if i <= j && s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func isValidOfIsPalindrome(b byte) bool {
+	return (b >= 'a' && b <= 'z') || (b >= '0' && b <= '9') || (b >= 'A' && b <= 'Z')
 }
